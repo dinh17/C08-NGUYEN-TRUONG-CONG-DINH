b8fcb06ad4ab8a4bdd2b832f6ca0fa6baedc95d9
dinh17 <nguyentruongcongdinh@gmail.com>
2022-10-06 21:05:30 +0700

    up_bai_tap

--- module2-C08/src/ss3_arrays_and_methods_in_Java/exercise/AddElementInArray.java
+++ module2-C08/src/ss3_arrays_and_methods_in_Java/exercise/AddElementInArray.java
@@ -3,7 +3,7 @@ package ss3_arrays_and_methods_in_Java.exercise;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ThemPhanTuVaoMang {
+public class AddElementInArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- module2-C08/src/ss3_arrays_and_methods_in_Java/exercise/DeleteElementInArray.java
+++ module2-C08/src/ss3_arrays_and_methods_in_Java/exercise/DeleteElementInArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class XoaPhanTuKhoiMang {
+public class DeleteElementInArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập mảng gồm n phần tử ");
