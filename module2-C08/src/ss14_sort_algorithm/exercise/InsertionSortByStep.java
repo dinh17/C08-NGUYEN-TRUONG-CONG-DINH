37a96b111166da201a5b32ec6697d75b1a44a517
dinh17 <nguyentruongcongdinh@gmail.com>
2022-10-24 15:26:07 +0700

    up_bai_tap

---
+++ module2-C08/src/ss14_sort_algorithm/exercise/InsertionSortByStep.java
@@ -0,0 +1,43 @@
+package ss14_sort_algorithm.exercise;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSortByStep {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array size:");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter " + array.length + " values:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.print("Your input array: ");
+        for (int value : array) {
+            System.out.print(value + "\t");
+        }
+        System.out.println("\nBegin sort processing...");
+        insertionSort(array);
+
+
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                System.out.println("thực thi : "+Arrays.toString(array));
+                j = j - 1;
+            }
+
+            array[j + 1] = key;
+        }
+        System.out.println(" kết quả " + Arrays.toString(array));
+    }
+
+}
